938486693ba265090c27245c4b6f545c524b9071
Mysterious Helper <111454500+YaBoyMH@users.noreply.github.com>
2022-09-22 23:02:40 -0500

    Create Player-Info-Obtainer.js

---
+++ DummyPotato-Blooket-Hacks/Player-Info-Obtainer.js
@@ -0,0 +1,4 @@
+var whatPlayer = prompt('What is the name of the player you want to get the info of?');
+var doUrl = window.open('https://api.blooket.com/api/users?name=' + whatPlayer);
+
+Also yes the code is two lines long lol. This just gives you info about them not there login or anything like that.
